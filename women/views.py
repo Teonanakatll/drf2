79a1394c5b122d9dc230699bf219e87c4b90e83d
Teonanakatll <105820090+Teonanakatll@users.noreply.github.com>
2024-07-08 20:35:57 +0300

    базовый класс Serializer

--- women/views.py
+++ women/views.py
@@ -10,16 +10,23 @@ from .serializers import WomenSerializer
 
 class WomenAPIView(APIView):
     def get(self, request):
-        lst = Women.objects.all().values()
-        return Response({'posts': list(lst)})
+        w = Women.objects.all()
+        # так как сериализатор будет обрабатывать не одну а список записей указываем many=True,
+        # и обращаемся к колекции data который будет представлять собой словарь преобразованных данных из таблицы Women
+        return Response({'posts': WomenSerializer(w, many=True).data})
 
     def post(self, request):
+        # создаём сериализатор на основе донных поступивших с пост запросом
+        serializer = WomenSerializer(data=request.data)
+        # проверяем коректность переданных данных
+        serializer.is_valid(raise_exception=True)
+
         post_new = Women.objects.create(
             title=request.data['title'],
             content=request.data['content'],
             cat_id=request.data['cat_id']
         )
-        return Response({'post': model_to_dict(post_new)})
+        return Response({'post': WomenSerializer(post_new).data})
 
 
 # class WomenAPIView(generics.ListAPIView):
