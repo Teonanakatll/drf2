b7bd9adbb77711af27062a1449041458a09fffef
Teonanakatll <105820090+Teonanakatll@users.noreply.github.com>
2024-07-09 09:47:12 +0300

    базовый класс Serializer

--- women/views.py
+++ women/views.py
@@ -20,13 +20,26 @@ class WomenAPIView(APIView):
         serializer = WomenSerializer(data=request.data)
         # проверяем коректность переданных данных
         serializer.is_valid(raise_exception=True)
+        # вызывает метод create() сериализатора
+        serializer.save()
 
-        post_new = Women.objects.create(
-            title=request.data['title'],
-            content=request.data['content'],
-            cat_id=request.data['cat_id']
-        )
-        return Response({'post': WomenSerializer(post_new).data})
+        return Response({'post': serializer.data})
+
+    def put(self, request, *args, **kwargs):
+        # из переданных данных берём pk
+        pk = kwargs.get("pk", None)
+        if not pk:
+            return Response({"error": "Method PUT not allowed"})
+
+        try:
+            instanse = Women.objects.get(pk=pk)
+        except:
+            return Response({"error": "Object does not exists"})
+
+        serializer = WomenSerializer(data=request.data, instanse=instanse)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response({"post": serializer.data})
 
 
 # class WomenAPIView(generics.ListAPIView):
