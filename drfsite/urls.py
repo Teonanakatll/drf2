ea1310ff1210abe3b42eb4335fc0abdda10923b6
Teonanakatll <105820090+Teonanakatll@users.noreply.github.com>
2024-07-10 17:12:20 +0300

    ViewSet и ModelViewSet

--- drfsite/urls.py
+++ drfsite/urls.py
@@ -15,13 +15,27 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
-from women.views import WomenAPIList, WomenAPIUpdate, WomenAPIDetailView  # WomenAPIView
+from women.views import WomenViewSet  # WomenAPIList, WomenAPIUpdate, WomenAPIDetailView, WomenAPIView
+from rest_framework import routers
+
+
+router = routers.SimpleRouter()
+# регистрируем вьюсет в роутере, придумываем префикс и укаываем вьюсет
+router.register(r'women', WomenViewSet)
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('api/v1/womenlist/', WomenAPIList.as_view()),
-    path('api/v1/womenlist/<int:pk>/', WomenAPIUpdate.as_view()),
-    path('api/v1/womendetail/<int:pk>/', WomenAPIDetailView.as_view()),
+    path('admin/', admin.site.urls),  # http://127.0.0.1:8000/api/v1/women/
+    path('api/v1/', include(router.urls)),
+
+
+    # во вьюсетах можно указывать метод обработки запроса и
+    # метод который будет вызываться в самом вьюсете
+    # path('api/v1/womenlist/', WomenViewSet.as_view({'get': 'list'})),
+    # path('api/v1/womenlist/<int:pk>/', WomenViewSet.as_view({'put': 'update'})),
+
+    # path('api/v1/womenlist/', WomenAPIList.as_view()),
+    # path('api/v1/womenlist/<int:pk>/', WomenAPIUpdate.as_view()),
+    # path('api/v1/womendetail/<int:pk>/', WomenAPIDetailView.as_view()),
 ]
