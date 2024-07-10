36eb8bde2983db27902486c6d8471e697f26273e
Teonanakatll <105820090+Teonanakatll@users.noreply.github.com>
2024-07-10 13:08:04 +0300

    UpdateAPIView, классы рендеров

--- drfsite/settings.py
+++ drfsite/settings.py
@@ -124,3 +124,12 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+REST_FRAMEWORK = {
+    # классы рендеров данных
+    'DEFAULT_RENDERER_CLASSES': [
+        'rest_framework.renderers.JSONRenderer',
+        # api браузера
+        # 'rest_framework.renderers.BrowsableAPIRenderer',
+    ]
+}
--- women/views.py
+++ women/views.py
@@ -12,53 +12,62 @@ class WomenAPIList(generics.ListCreateAPIView):
     queryset = Women.objects.all()
     serializer_class = WomenSerializer
 
-class WomenAPIView(APIView):
-    def get(self, request):
-        w = Women.objects.all()
-        # так как сериализатор будет обрабатывать не одну а список записей указываем many=True,
-        # и обращаемся к колекции data который будет представлять собой словарь преобразованных данных из таблицы Women
-        return Response({'posts': WomenSerializer(w, many=True).data})
 
-    def post(self, request):
-        # создаём сериализатор на основе донных поступивших с пост запросом
-        serializer = WomenSerializer(data=request.data)
-        # проверяем коректность переданных данных
-        serializer.is_valid(raise_exception=True)
-        # вызывает метод create() сериализатора
-        serializer.save()
+class WomenAPIUpdate(generics.UpdateAPIView):
+    queryset = Women.objects.all()
+    serializer_class = WomenSerializer
 
-        return Response({'post': serializer.data})
+class WomenAPIDetailView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Women.objects.all()
+    serializer_class = WomenSerializer
 
-    def put(self, request, *args, **kwargs):
-        # из переданных данных берём pk
-        pk = kwargs.get("pk", None)
-        if not pk:
-            return Response({"error": "Method PUT not allowed"})
-
-        try:
-            instance = Women.objects.get(pk=pk)
-        except:
-            return Response({"error": "Object does not exists"})
-
-        serializer = WomenSerializer(data=request.data, instance=instance)
-        serializer.is_valid(raise_exception=True)
-        serializer.save()
-        return Response({"post": serializer.data})
-
-    def delete(self, request, *args, **kwargs):
-        pk = kwargs.get("pk", None)
-        if not pk:
-            return Response({"error": "Method DELETE not allowed"})
-
-        try:
-            instance = Women.objects.get(pk=pk)
-        except:
-            return Response({"error": "Object does not exists"})
-
-        instance.delete()
-
-
-        return Response({"post": f'Delete post - {pk}'})
+# class WomenAPIView(APIView):
+#     def get(self, request):
+#         w = Women.objects.all()
+#         # так как сериализатор будет обрабатывать не одну а список записей указываем many=True,
+#         # и обращаемся к колекции data который будет представлять собой словарь преобразованных данных из таблицы Women
+#         return Response({'posts': WomenSerializer(w, many=True).data})
+#
+#     def post(self, request):
+#         # создаём сериализатор на основе донных поступивших с пост запросом
+#         serializer = WomenSerializer(data=request.data)
+#         # проверяем коректность переданных данных
+#         serializer.is_valid(raise_exception=True)
+#         # вызывает метод create() сериализатора
+#         serializer.save()
+#
+#         return Response({'post': serializer.data})
+#
+#     def put(self, request, *args, **kwargs):
+#         # из переданных данных берём pk
+#         pk = kwargs.get("pk", None)
+#         if not pk:
+#             return Response({"error": "Method PUT not allowed"})
+#
+#         try:
+#             instance = Women.objects.get(pk=pk)
+#         except:
+#             return Response({"error": "Object does not exists"})
+#
+#         serializer = WomenSerializer(data=request.data, instance=instance)
+#         serializer.is_valid(raise_exception=True)
+#         serializer.save()
+#         return Response({"post": serializer.data})
+#
+#     def delete(self, request, *args, **kwargs):
+#         pk = kwargs.get("pk", None)
+#         if not pk:
+#             return Response({"error": "Method DELETE not allowed"})
+#
+#         try:
+#             instance = Women.objects.get(pk=pk)
+#         except:
+#             return Response({"error": "Object does not exists"})
+#
+#         instance.delete()
+#
+#
+#         return Response({"post": f'Delete post - {pk}'})
 
 
 # class WomenAPIView(generics.ListAPIView):
